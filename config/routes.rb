4cbb756a72ab7d7f4dca789bfebc5cfc6bb4c04f
asaki kusano <darumade4@gmail.com>
2020-06-07 12:40:36 +0900

    session機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
  resources :pictures do
    collection do
      post :confirm
    end
  end
  resources :users, only: [:new, :show, :edit, :create, :update]
+ resources :sessions, only: [:new, :create, :destroy]
 end
